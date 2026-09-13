ffee22a8d2807f5fbd8f4366a8f058b147a36d94
Lantean <jariknik94@gmail.com>
2026-09-13 21:05:57 +0300

    update page team

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -180,8 +180,8 @@ const team = [
   {
     name: "Валентина Ивановна Зайцева",
     role: "Ведущий аудитор",
-    photo: "/images/team/zaitseva1.jpg",
-    documentPhoto: "/images/team/zaitsevaFace.jpg",
+    photo: "/images/team/zaitseva2.jpg",
+    documentPhoto: "/images/team/zaitsevaFace2.jpg",
 
     text: (
       <>
