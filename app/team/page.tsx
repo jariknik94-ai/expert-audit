2d40da8735388a3cbc8a596e8dae5aedc60f6cf4
Lantean <jariknik94@gmail.com>
2026-09-25 19:21:34 +0300

    скорректировано наименование аттестаттов

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -78,7 +78,7 @@ const team = [
       {
         title: "Аттестат аудитора",
         files: [
-          "/documents/team/bugasova/certificate/Auditor Qualification Certificate Bugasova, 2010.jpg",
+          "/documents/team/bugasova/certificate/Auditor Qualification Certificate Bugasova 2010.jpg",
         ],
       },
       {
@@ -136,7 +136,7 @@ const team = [
       {
         title: "Аттестат аудитора",
         files: [
-          "/documents/team/dorozhkina/certificate/Auditor Qualification Certificate Dorozhkin, 1996.jpg",
+          "/documents/team/dorozhkina/certificate/Auditor Qualification Certificate Dorozhkin 1996.jpg",
         ],
       },
       {
